5eb61ce27039128d5cafe90da13bc6d799740db6
KidusAlemayehu <kidusalemayehu705@gmail.com>
2022-04-19 18:37:07 +0300

    user model views edited

--- interest/views.py
+++ interest/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from rest_framework.generics import ListAPIView, CreateAPIView
 from .serializers import InterestSerializer
 from rest_framework import permissions
+from accounts.models import User
 from .models import Interest
 
 # Create your views here.
@@ -11,5 +12,13 @@ class AddInterest(CreateAPIView):
 
 class ListInterest(ListAPIView):
 	permission_classes = (permissions.AllowAny,)
-	queryset = Interest.objects.all()
 	serializer_class = InterestSerializer
+
+class AddInterestToUser(CreateAPIView):
+	permission_classes = (permissions.IsAuthenticated,)
+	def post(self, request, id):
+		email = request.user
+		user = User.objects.get(email=email)
+		interest = Interest.objects.get(id=id)
+		user.interests.add
+
